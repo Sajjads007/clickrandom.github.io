acd945c21e14234a1213bbb84bbab9d45d54c2f1
Sajjads007 <sajjads0307@gmail.com>
2024-08-23 10:34:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+let heading = document.getElementById("heading");
+document.addEventListener("keypress", (event) => {
+  if (event.code == "KeyQ") {
+    heading.innerText = "You pressed Q";
+  } else if (event.code == "KeyW") {
+    heading.innerText = "You pressed W";
+  } else if (event.code == "KeyE") {
+    heading.innerText = "You pressed E";
+  } else if (event.code == "KeyR") {
+    heading.innerText = "You pressed R";
+  } else if (event.code == "KeyT") {
+    heading.innerText = "You pressed T";
+  } else if (event.code == "KeyY") {
+    heading.innerText = "You pressed Y";
+  } else if (event.code == "KeyU") {
+    heading.innerText = "You pressed U";
+  } else if (event.code == "KeyI") {
+    heading.innerText = "You pressed I";
+  } else if (event.code == "KeyO") {
+    heading.innerText = "You pressed O";
+  } else if (event.code == "KeyP") {
+    heading.innerText = "You pressed P";
+  } else if (event.code == "KeyA") {
+    heading.innerText = "You pressed A";
+  } else if (event.code == "KeyS") {
+    heading.innerText = "You pressed S";
+  } else if (event.code == "KeyD") {
+    heading.innerText = "You pressed D";
+  } else if (event.code == "KeyF") {
+    heading.innerText = "You pressed F";
+  } else if (event.code == "KeyG") {
+    heading.innerText = "You pressed G";
+  } else if (event.code == "KeyH") {
+    heading.innerText = "You pressed H";
+  } else if (event.code == "KeyJ") {
+    heading.innerText = "You pressed J";
+  } else if (event.code == "KeyK") {
+    heading.innerText = "You pressed K";
+  } else if (event.code == "KeyL") {
+    heading.innerText = "You pressed L";
+  } else if (event.code == "KeyZ") {
+    heading.innerText = "You pressed X";
+  } else if (event.code == "KeyC") {
+    heading.innerText = "You pressed C";
+  } else if (event.code == "KeyV") {
+    heading.innerText = "You pressed V";
+  } else if (event.code == "KeyA") {
+    heading.innerText = "You pressed A";
+  } else if (event.code == "KeyA") {
+    heading.innerText = "You pressed A";
+  } else if (event.code == "KeyA") {
+    heading.innerText = "You pressed A";
+  } else if (event.code == "KeyA") {
+    heading.innerText = "You pressed A";
+  } else if (event.code == "KeyB") {
+    heading.innerText = "You pressed B";
+  } else if (event.code == "KeyN") {
+    heading.innerText = "You pressed N";
+  } else if (event.code == "KeyM") {
+    heading.innerText = "You pressed M";
+  } else {
+    heading.innerText = "If you move you're GAY";
+  }
+});
